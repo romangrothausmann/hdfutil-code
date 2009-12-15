16c7a05fedb47ff624c9483757b39dea6a1c0099
fredrior <fredrior@478d8701-377a-4c2a-8c36-5981bb6be2ce>
2009-12-15 14:55:17 +0000

    added initial webpage.

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+   <title>Hdf5 utilities</title>
+   <link rel="Stylesheet" type="text/css" href="style.css">
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+<body>
+
+<h1>Hdf5 utilities</h1>
+Hdf5 utilities is a collection of utilities to ease the exchange of HDF5 files between C/C++, Matlab and Python. The long term goal is integration into the main HDF5 distribution.
+<p>
+
+License: <a href="http://www.opensource.org/licenses/bsd-license.php">BSD</a>.
+
+
+
+Link to <a href="http://sourceforge.net/projects/hdfutil/">project page</a> on sourceforge.
+
+<br><br>
+<p align="right">
+Last modified <?php echo date("Y-m-d", getlastmod()); ?>.
+</p>
+
+</body>
+</html>
