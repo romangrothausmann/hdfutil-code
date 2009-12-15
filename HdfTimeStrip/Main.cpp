22ed851482259589b28cf025dcb3695bbf982c4a
fredrior <fredrior@478d8701-377a-4c2a-8c36-5981bb6be2ce>
2009-12-15 15:03:58 +0000

    added time-stripping application.

---
+++ HdfTimeStrip/Main.cpp
@@ -0,0 +1,112 @@
+// auto-linking of HDF5 libraries
+#ifdef _WIN32
+#  ifdef _DEBUG
+#    ifdef HDF5CPP_USEDLL
+#      pragma comment(lib,"hdf5ddll.lib")
+#      pragma comment(lib,"hdf5_cppddll.lib")
+#    else
+#      pragma comment(lib,"hdf5d.lib")
+#      pragma comment(lib,"hdf5_cppd.lib")
+#    endif
+#  else
+#    ifdef HDF5CPP_USEDLL
+#      pragma comment(lib,"hdf5dll.lib")
+#      pragma comment(lib,"hdf5_cppdll.lib")
+#    else
+#      pragma comment(lib,"hdf5.lib")
+#      pragma comment(lib,"hdf5_cpp.lib")
+#    endif
+#  endif
+#endif
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <exception>
+#include <stdio.h>
+#include <H5Cpp.h>
+
+/** Copy each scalar/vector/matrix dataset element. */
+static void parse_dataset (const H5::DataSet & in_dset, H5::DataSet & out_dset) {
+    H5::DataType dtype = in_dset.getDataType();
+    hsize_t      size  = in_dset.getStorageSize();
+
+    std::vector<unsigned char> buffer((unsigned int)size);
+	in_dset.read  (&buffer[0], dtype);
+	out_dset.write(&buffer[0], dtype);
+}
+
+/** Read/write each group of datasets. */
+static void parse_group (const H5::CommonFG & in_grp, H5::CommonFG & out_grp) {
+    using namespace H5;
+    hsize_t N = in_grp.getNumObjs();
+    for (size_t i = 0; i < N; i++) {
+        std::string name = in_grp.getObjnameByIdx(i);
+        H5G_obj_t   type = in_grp.getObjTypeByIdx(i);
+
+        if        (type == H5G_GROUP) {
+            Group in_child  = in_grp.openGroup(name);
+            Group out_child = out_grp.createGroup(name);
+            // recursive parsing of child groups
+            parse_group(in_child, out_child);
+        } else if (type == H5G_DATASET) {
+            DataSet   in_dset = in_grp.openDataSet(name);
+            DataType  dtype   = in_dset.getDataType();
+            DataSpace dspace  = in_dset.getSpace();
+
+			// disable time-stamping of new datasets
+			DSetCreatPropList plist;
+			H5Pset_obj_track_times(plist.getId(), false);
+            // copy dataset content
+            DataSet out_dset = out_grp.createDataSet(name, dtype, dspace, plist);
+            parse_dataset(in_dset, out_dset);
+        } else
+            throw std::exception("Unsupported object type");
+    }
+}
+
+/** Name of temp file used when no output is specified. */
+const static std::string TEMP_FILE = "temp_hdftimestrip.h5";
+
+/** Program entry point. */
+int main (int argc, const char* argv[]) {
+    using namespace std;
+    cout << "HdfTimeStrip by Fredrik Orderud, 2009. Strips time-stamp information from HDF5 files, "
+         << "so that HDF5 files of identical content become binary duplicates." << endl;
+
+ 	if (argc < 2) {
+		cerr << "USAGE: HdfTimeStrip src_file [dst_file]" << endl;
+		return -1;
+	}
+
+	// determine input/output filenames
+	string in_file  = argv[1];
+	string out_file = TEMP_FILE;
+	if (argc >= 3)
+		out_file = argv[2];
+
+    cout << "Loading " << in_file << "..." << endl;
+	try {
+		// parse input & generate output
+		H5::H5File input (in_file,  H5F_ACC_RDONLY);
+		H5::H5File output(out_file, H5F_ACC_TRUNC);
+		parse_group(input, output);
+    } catch (const std::exception & e) {
+        cerr << e.what();
+        remove(out_file.c_str());
+        return -1;
+    }
+
+	// replace input file if no output is specifiec
+	if (out_file == TEMP_FILE) {
+        cout << "Writing to " << in_file << "..." << endl;
+		int result  = remove(in_file.c_str());
+		    result |= rename(out_file.c_str(), in_file.c_str());
+		return result;
+	} else
+        cout << "Writing to " << out_file << "..." << endl;
+
+
+    cout << "[done]" << endl;
+    return 0;
+}
---
+++ HdfTimeStrip/README.txt
@@ -0,0 +1,6 @@
+== HdfTimeStrip ==
+HdfTimeStrip, by Fredrik Orderud 2009.
+
+Command-line utility to strip time-stamp information from HDF5 files, so that HDF5 files of identical content become binary duplicates.
+
+TIP: Buid HdfTimeStrip against static HDF5 libraries to avoid any DLL dependencies.
